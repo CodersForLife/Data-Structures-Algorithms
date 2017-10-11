0271ec2ad7dab36ee7a61a16f738e15b586e0d3e
Luis Bonilla <luis.bonillaii93@gmail.com>
2017-10-10 21:10:57 -0500

    Added quicksort algorithm
    Language:C++

---
+++ Sorting/Quicksort.cc
@@ -0,0 +1,26 @@
+void quickSort(int arr[], int left, int right) {
+      int i = left, j = right;
+      int tmp;
+      int pivot = arr[(left + right) / 2];
+ 
+      /* partition */
+      while (i <= j) {
+            while (arr[i] < pivot)
+                  i++;
+            while (arr[j] > pivot)
+                  j--;
+            if (i <= j) {
+                  tmp = arr[i];
+                  arr[i] = arr[j];
+                  arr[j] = tmp;
+                  i++;
+                  j--;
+            }
+      };
+ 
+      /* recursion */
+      if (left < j)
+            quickSort(arr, left, j);
+      if (i < right)
+            quickSort(arr, i, right);
+}
