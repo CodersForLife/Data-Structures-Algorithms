e0fd5f77d484a9584477cd18f651d55a54411be2
090max <32358539+090max@users.noreply.github.com>
2017-10-11 11:33:47 +0530

    bubblesort.c
    Added bubble sort In C language

---
+++ Sorting/bubble sort.c.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int max,i,n,j;
+    printf("Enter the size of array");
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    for(j=0;j<n;j++)
+    {if(a[i]<a[j])
+   {max=a[i];
+    a[i]=a[j];
+    a[j]=max;
+    }
+    }
+   for(i=0;i<n;i++)
+   printf("%d ",a[i]);
+    return 0;
+}
