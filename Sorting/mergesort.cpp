8ea34e5f76ff905131f8dcbab2312bb7b744d95e
ungps <ungureanupaulsebastian@gmail.com>
2017-10-12 20:37:40 +0300

    Mergesort improvement

--- Sorting/mergesort.cpp
+++ Sorting/mergesort.cpp
@@ -1,76 +1,42 @@
-#include<iostream>
-#include<cmath>
-using namespace std;
-void merge(int A[],int p,int q,int r)
-{
-	int i,j,x,y;
-	int n1=q-p+1;int n2=r-q;
-	int B[n1],C[n2];
-	for(i=0;i<n1;i++)
-	{
-		B[i]=A[p+i];
-	}
-	for(x=0;x<n2;x++)
-	{
-		C[x]=A[q+1+x];
-	}int k;
-	i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = p; // Initial index of merged subarray
-    while (i < n1 && j < n2)
-    {
-        if (B[i] <= C[j])
-        {
-            A[k] = B[i];
-            i++;
-        }
-        else
-        {
-            A[k] = C[j];
-            j++;
-        }
-        k++;
+#include <stdio.h>
+
+#define NX 500010
+
+int N, A[NX], B[NX];
+
+void mergeSort(int left, int right) {
+    int middle = (left + right) >> 1, i, j, k;
+
+    if (left == right) {
+        return;
     }
- 
-    while (i < n1)
-    {
-        A[k] = B[i];
-        i++;
-        k++;
+
+    mergeSort(left, middle);
+    mergeSort(middle + 1, right);
+
+    for (i = left, j = middle + 1, k = left; i <= middle || j <= right;) {
+        if (j > right || (i <= middle && A[i] < A[j])) {
+            B[k++] = A[i++];
+        } else {
+            B[k++] = A[j++];
+        }
     }
- 
-    while (j < n2)
-    {
-        A[k] = C[j];
-        j++;
-        k++;
+    for (k = left; k <= right; k++) {
+        A[k] = B[k];
     }
 }
 
+int main() {
+    int i;
 
+    scanf("%d", &N);
+    for (i = 1; i <= N; i++)
+        scanf("%d", A + i);
 
-void mergesort(int A[],int p,int r)
-{int q;
-	if(p>=r)
-	{
-		return;
-	}
-	else
-	{
-	     q=(p+r)/2;
-		 mergesort(A,p,q);
-		 mergesort(A,q+1,r);
-		 merge(A,p,q,r);
-	}
-}
+    mergeSort(1, N);
 
-int main()
-{
-	int A[5],a;
-	for(a=0;a<5;a++)
-	cin>>A[a];
-	mergesort(A,0,4);
-	for(a=0;a<5;a++)
-	cout<<A[a]<<" ";
-	
+    for (i = 1; i <= N; i++)
+        printf("%d ", A[i]);
+
+    return 0;
 }
