b054da1ceac057214d81894edc25364859457d6e
kapil Israni <32358539+090max@users.noreply.github.com>
2017-10-13 23:45:55 +0530

    Update main.c.c

--- main.c.c
+++ main.c.c
@@ -8,16 +8,18 @@ int main()
     scanf("%d",&n);
     int a[n];
     for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
+       scanf("%d",&a[i]);
     for(i=0;i<n;i++)
-    for(j=0;j<n;j++)
-    {if(a[i]<a[j])
-   {max=a[i];
-    a[i]=a[j];
-    a[j]=max;
-    }
-    }
+       for(j=0;j<n;j++)
+       {    
+           {if(a[i]<a[j])
+               {max=a[i];
+                a[i]=a[j];
+                a[j]=max;
+                }
+               }
+       }
    for(i=0;i<n;i++)
-   printf("%d ",a[i]);
+       printf("%d ",a[i]);
     return 0;
 }
