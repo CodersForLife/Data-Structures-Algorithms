9c927dac51169dd95575d01a80b9e8ce4e71973d
ArunaMaurya221B <aruna.maurya12@gmail.com>
2017-10-13 21:11:02 +0530

    Binary Search in C

--- Searching/Binary-Search.c
+++ Searching/Binary-Search.c
@@ -1,24 +1,27 @@
-#include<stdio.h>
-int main(){
-	int n;
-	printf("\nEnter the number pf elements:");
-	scanf("%d",&n);
-	printf("\nEnter the elements:");
-	int ar[n];
-	int i;
-	for(i=0;i<n;i++)
-		scanf("%d",&ar[i]);
-	printf("\nEnter the element to be searched for:");
-	int element;
-	scanf("%d",element);
-	int beg=0,mid,last=n-1;
-	mid=(beg+last)/2;
-	while(beg<=last){
-		if(ar[mid]==element)
-			printf("\nFound element");
-		else if(ar[mid]>element)
-			last=mid-1;
-		else
-			beg=mid+1;
-	}
+#include <stdio.h>
+
+int binarySearch(int arr[], int beg, int last, int element)  // returns the position of the element if the search is successfull, otherwise returns -1.
+{
+   if (beg<=last)
+   {
+        int mid = (beg+last)/2;
+        if (arr[mid] == x) 
+        return mid;
+        else if (arr[mid] > x) 
+        return binarySearch(arr,beg, mid-1, x);
+         else
+        return binarySearch(arr, mid+1, last, x);
+   }
+   return -1;
+}
+ 
+int main()
+{
+   int arr[] = {2, 3, 4, 10, 40};
+   int n = sizeof(arr)/ sizeof(arr[0]);
+   int x = 10;
+   int result = binarySearch(arr, 0, n-1, x);
+   (result == -1)? printf("Element is not present in array")
+                 : printf("Element is present at index %d", result);
+   return 0;
 }
