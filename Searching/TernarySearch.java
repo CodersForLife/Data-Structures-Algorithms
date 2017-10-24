9e7c4c206b1da2fc9bb76f47d2d9bef6873acd40
atkalikar <Atkalikar@sky.optymyze.com>
2017-10-24 11:34:42 +0530

    Added documentation and removed main method.

--- Searching/TernarySearch.java
+++ Searching/TernarySearch.java
@@ -1,5 +1,17 @@
+/**
+ * Program to demonstrate ternary search.
+ */
 public class TernarySearch {
 
+    /**
+     * searches given searchValue in an array.
+     *
+     * @param arr         Collection of data.
+     * @param searchValue Value to be searched in given collection of data.
+     * @param start       Start index; the searching will begin from this index in array.
+     * @param end         End index; this the upper bound for search an element in array.
+     * @return Index of element in array.
+     */
     public static int ternarySearch(int[] arr, int searchValue, int start, int end) {
         if (start > end) {
             return -1;
@@ -18,11 +30,5 @@ public class TernarySearch {
         } else {
             return ternarySearch(arr, searchValue, mid1, mid2);
         }
-    }
-
-    public static void main(String[] args) {
-        int[] numbers = {1, 2, 3};
-        System.out.println("index - " + ternarySearch(numbers, 2, 0, 2));
-
     }
 }
