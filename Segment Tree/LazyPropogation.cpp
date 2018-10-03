71375599d4a4a02fb53f64c498fb238595f504f9
Kumar Prateek <121.kp27@gmail.com>
2018-10-03 13:37:08 +0530

    Added lazy propogation

--- Segment Tree/LazyPropogation.cpp
+++ Segment Tree/LazyPropogation.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
+#define ll long long int
 #define f(i,s,e) for(ll i=s;i<=e;i++)
-#define aspro ios_base::sync_with_stdio(0);cin.tie(0);
+#define io ios_base::sync_with_stdio(0);cin.tie(NULL);
 #define pb push_back
 #define pii pair<int,int>
 #define si(a) scanf("%d",&a)
@@ -58,17 +58,13 @@ void update(int node,int st,int en,int l,int r,int val )
     update( 2*node,st, mid,l,r,val );
     update( 2*node+1,mid+1,en,l,r,val );
     if ( st != en )
-    {
         tree[node]=tree[2*node]+tree[2*node+1];
-    }
 }
 
 int query( int node, int st, int en, int l, int r )
 {
     if ( st > en || st > r || en < l )
-    {
         return 0;
-    }
     refresh( node, st, en );
     if(st>=l&&en<=r)  return tree[node];
     int mid=(st+en)/2;
@@ -77,10 +73,12 @@ int query( int node, int st, int en, int l, int r )
     return x+y;
 }
 int main()
-{
+{   
+    io
     int n,m,x,y,type,v,ans;
     cin>>n;
-    f(i,1,n) cin>>arr[i];
+    f(i,1,n) 
+    cin>>arr[i];
     build(1,1,n);
     f(i,1,2*n-1) cout<<tree[i]<<" ";cout<<endl;
     cin>>m;
@@ -92,7 +90,6 @@ int main()
         cin>>type>>x>>y;
         if(type)
         {
-            //ans=query(1,1,n,x,y);
             ans=query(1,1,n,x,y);
             printf("%d\n",ans);
         }
