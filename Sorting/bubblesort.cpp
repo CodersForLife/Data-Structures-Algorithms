4f03f67da7b57decda8214d51f0b26f79e651122
Jitendra Prajapati <jp803333@gmail.com>
2019-10-20 20:32:30 +0530

    In bubblesort added the swap counter which when zero(means the array is sorted) breaks the loop

--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -14,15 +14,18 @@ int main()
  
   for (c = 0 ; c < ( n - 1 ); c++)
   {
+    int count = 0;//Swap counter
     for (d = 0 ; d < n - c - 1; d++)
     {
       if (array[d] > array[d+1]) /* For decreasing order use < */
       {
+        count++;
         swap       = array[d];
         array[d]   = array[d+1];
         array[d+1] = swap;
       }
     }
+    if(count==0) break;//Noneed to Run the loop when there are no more swaps.
   }
  
   printf("Sorted list in ascending order:\n");
